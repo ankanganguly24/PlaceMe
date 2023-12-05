b164b9ad5974f18257a0a5d669d619ae8e33cb94
Ankan <ankanganguly24@gmail.com>
2023-12-05 22:26:44 +0530

    Refactor email validation schema

--- src/lib/validations/auth.js
+++ src/lib/validations/auth.js
@@ -5,13 +5,11 @@ export const usernameSchema = z
     .min(3, "Username must be at least 3 characters long")
     .max(20, "Username must be at most 20 characters long");
 
-export const emailSchema = z
-    .string()
-    .email("Invalid email address")
-    .regex(
-        /^[a-zA-Z0-9_.+-]+@inspiria\.edu\.in$/,
-        "Email must be a valid Inspiria email address"
-    );
+export const emailSchema = z.string().email("Invalid email address");
+// .regex(
+//     /^[a-zA-Z0-9_.+-]+@inspiria\.edu\.in$/,
+//     "Email must be a valid Inspiria email address"
+// );
 
 export const signUpSchema = z.object({
     username: usernameSchema,
